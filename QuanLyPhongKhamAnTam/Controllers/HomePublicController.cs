fa9a0e0b1a464a217d82eed113ea71d3eae34772
Võ Hoàng Long <23DH114438@st.huflit.edu.vn>
2025-05-26 09:28:28 +0700

    Add project files.

---
+++ QuanLyPhongKhamAnTam/Controllers/HomePublicController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyPhongKhamAnTam.Controllers
+{
+    public class HomePublicController : Controller
+    {
+        // GET: HomePublic
+        public ActionResult TrangchuPK()
+        {
+            return View();
+        }
+        public ActionResult DichvuPK()
+        {
+            return View();
+        }
+        public ActionResult GioithieuPK()
+        {
+            return View();
+        }
+        public ActionResult KhamBHYT()
+        {
+            return View();
+        }
+        public ActionResult KhamSK()
+        {
+            return View();
+        }
+        public ActionResult Noisoi()
+        {
+            return View();
+        }
+        public ActionResult KhamSKDHDL()
+        {
+            return View();
+        }
+        public ActionResult TiemChung()
+        {
+            return View();
+        }
+        public ActionResult Noitongquat()
+        {
+            return View();
+        }
+        public ActionResult Ngoaitongquat()
+        {
+            return View();
+        }
+        public ActionResult Taimuihong()
+        {
+            return View();
+        }
+        public ActionResult Ranghammat()
+        {
+            return View();
+        }
+        public ActionResult Nhikhoa()
+        {
+            return View();
+        }
+        public ActionResult Sanphukhoa()
+        {
+            return View();
+        }
+        public ActionResult YHocCoTruyen()
+        {
+            return View();
+        }
+    }
+}
