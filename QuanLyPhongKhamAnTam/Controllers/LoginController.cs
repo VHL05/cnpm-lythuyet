e552205d2e8b5f8316521bb5cf90b984cbaaf3e3
hnahtIv <61571748+tuvithanh@users.noreply.github.com>
2025-06-21 15:27:24 +0700

    loginregister View
    view

---
+++ QuanLyPhongKhamAnTam/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+namespace QuanLyPhongKhamAnTam.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+            // Logic to authenticate user goes here
+            if (username == "admin" && password == "password") // Example condition
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.ErrorMessage = "Invalid username or password.";
+            return View();
+        }
+    }
+}
---
+++ QuanLyPhongKhamAnTam/Controllers/RegisterController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+namespace QuanLyPhongKhamAnTam.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Login
+        public ActionResult Register()
+        {
+            return View();
+        }
+        
+    }
+}
