3d8a8e77a7121102a425f456d62fa07ca68dd5e9
Võ Hoàng Long <23DH114438@st.huflit.edu.vn>
2025-06-21 11:40:59 +0700

    Area with controllers and views

---
+++ QuanLyPhongKhamAnTam/Areas/Staff/Controllers/StaffHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyPhongKhamAnTam.Areas.Staff.Controllers
+{
+    public class StaffHomeController : Controller
+    {
+        // GET: Staff/StaffHome
+        public ActionResult HomeStaff()
+        {
+            return View();
+        }
+    }
+}
